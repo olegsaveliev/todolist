d8e9d8e5ba046e8c3b357c93c04929b10006f259
osavelyev <osavelyev@gmail.com>
2022-04-17 12:56:08 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Oleg Savelyev on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+            ListView()
+            }
+        }
+    }
+}
